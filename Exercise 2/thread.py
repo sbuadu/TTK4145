2e1492b2db761f9f4a89b5dbd22b83a5d0cb2409
Stephanie Buadu <steffyquayson@hotmail.com>
2017-01-19 10:55:41 +0100

    Fixed the concurrency problem in the python code

--- Exercise 2/thread.py
+++ Exercise 2/thread.py
@@ -1,32 +1,40 @@
 
 from threading import Thread
+import Queue
 
-i = 0
 
-def someThreadFunction1():
+
+def someThreadFunction1(q):
 	for x in range(1000000):
-		global i
-		i+=1
+		while  not q.empty :
+		 	i = q.get() 
+		 	q.put(i+1)
 
-def someThreadFunction2():
+def someThreadFunction2(q):
 	for x in range(1000000):
-		global i
-		i-=1
+		while  not q.empty :
+		 	i = q.get() 
+		 	q.put(i-1)
+
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 
 
 
-def main():
-	someThread1 = Thread(target = someThreadFunction1, args = (),)
-	someThread2 = Thread(target = someThreadFunction2, args = (),)
+def main():	
+
+	q = Queue.LifoQueue(1)
+	q.put(0); 
+
+	someThread1 = Thread(target = someThreadFunction1, args = (q,))
+	someThread2 = Thread(target = someThreadFunction2, args = (q,))
 	someThread1.start()
 	someThread2.start()
     
 	someThread1.join()
 	someThread2.join()
-	print(i)
+	print q.get()
 
 
 main()
