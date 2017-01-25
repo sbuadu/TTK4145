6846adb58bef5843686c8d3033644fa18f139517
Joel Sågfors <sagforj1@aalto.fi>
2017-01-25 17:01:31 +0100

    Update on design

--- Project/elev_algo/copy_driver.sh
+++ Project/elev_algo/copy_driver.sh
@@ -4,5 +4,5 @@ cp ../driver/io.h driver;
 cp ../driver/channels.h driver;
 
 mkdir -p sim_server;
-cp ../simulator_2/server/* sim_server;
-cp ../simulator_2/simulator.con .;
+cp ../simulator/server/* sim_server;
+cp ../simulator/simulator.con .;
