35b0eb969ef687952eb4c7e3bdf74142ab0462ee
Sigurd Murad <muradsigurd@gmail.com>
2017-01-26 12:49:08 +0100

    Halfway done udp

--- Exercise 3/UDP.go
+++ Exercise 3/UDP.go
@@ -7,9 +7,23 @@ import (
 
 
 func main() {
-	packet, _ := net.ListenPacket("udp",":30000")
-	//addr, _ := hex.Dedoce(i, packet)
-	fmt.Println("Hello from the network module")
-	fmt.Printf("%p\n", packet)
+	// udp ip addr
+	server, _ := net.ResolveUDPAddr("udp", "129.241.187.255:30000")
+	// create socket
+	conn, _ := net.ListenUDP("udp", server)
+	// create buffer to store msg
+	buffer := make([]byte, 1024)
+	buffer2 := make([]byte, 1024)
+	// read from server addr
+	conn2, _ := net.DialUDP("udp", server, "129.241.187.255:20009")
+	conn2.WriteToUDP("Hello",server)
+
+	for {
+		n, addr, _ := conn.ReadFromUDP(buffer)
+		n1, addr2, _ := conn2.ReadFromUDP(buffer2)
+		// print info
+		fmt.Println(string(buffer[:n]))
+		fmt.Println(addr)
+}
 }
 
