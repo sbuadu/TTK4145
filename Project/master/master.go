e55c8864ac9758eafec1df59dbc3caacd8fdb328
Joel Sågfors <sagforj1@aalto.fi>
2017-03-13 12:06:47 +0100

    Trying out remotely starting master

--- Project/master/master.go
+++ Project/master/master.go
@@ -210,9 +210,9 @@ func MasterLoop(isBackup bool) {
                     slaveAliveChan <- slaveAlive
                     if slaveIPs[i] != myIP {
                         backupIP = slaveIPs[i]
-                        fmt.Println("Expexting a backup on IP", backupIP)
-                        //spawnMasterBackup := exec.Command("bash","./startSlave.sh",backupIP,"-startMasterBackup")
-                        //spawnMasterBackup.Start()
+                        fmt.Println("Starting a backup on IP", backupIP)
+                        spawnMasterBackup := exec.Command("bash","./startProcess.sh",backupIP,"-startMasterBackup")
+                        spawnMasterBackup.Start()
                         break
                     }
                 }
